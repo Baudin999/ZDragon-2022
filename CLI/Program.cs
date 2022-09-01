5c5fb74c75658a7b175340af205e9abe796602e0
baudin999 <carlos.kelkboom@essent.nl>
2022-09-01 07:13:28 +0200

    The CLI now also works on OSX

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -9,6 +9,26 @@ string basePath =
         Environment.GetCommandLineArgs()[1] : 
         Directory.GetCurrentDirectory();
 
+var userProfileFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+Console.WriteLine(userProfileFolder);
+
+if (!Environment.OSVersion.ToString().ToLower().Contains("win") && basePath.StartsWith("~"))
+{
+    // remove the tilde, and the first starting /
+    // and combine that with the user profile path
+    // to get to the actual path on a unix/linux/osx machine
+    basePath = Path.Combine(userProfileFolder, basePath.Replace("~", "").Substring(1));
+}
+
+if (!Directory.Exists(basePath))
+{
+    Console.WriteLine($"{basePath} does not exist.");
+    return;
+}
+Console.WriteLine("Watching: " + basePath);
+
+
+
 using var fileWatcher = new FileWatcher();
 using var fileObserver = new FileObserver(basePath);
 var unsubscribe = fileWatcher.Subscribe(fileObserver);
