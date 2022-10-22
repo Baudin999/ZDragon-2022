425790a6c60d5edb2ace2d0da4c25f1e611dfbc7
baudin999 <ckelkboom@protonmail.com>
2022-10-22 09:37:43 +0200

    deleted the scratch file

---
+++ Compiler.Test/Scratch.cs
@@ -0,0 +1,16 @@
+﻿namespace Compiler.Test;
+
+public class Scratch
+{
+    [Fact]
+    public async void TestStickyComponents()
+    {
+        const string code = @"
+component Foo
+";
+        var zdragon = await new ZDragon().Compile(code);
+        // Assert.Equal(2, zdragon.Nodes.Count);
+        Assert.IsType<ComponentNode>(zdragon.Nodes[0]);
+        // Assert.IsType<ComponentNode>(zdragon.Nodes[1]);
+    }
+}
